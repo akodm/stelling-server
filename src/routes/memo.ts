7fbe6d25c4625d94734242450fa793ca1a929874
jojunmyeong <a8456452@naver.com>
2021-04-16 17:42:00 +0900

    타입스크립트 컨피그 파일 수정 중.

--- src/routes/memo.ts
+++ src/routes/memo.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Response } from "express";
 import express from 'express';
 import sequelize from '../sequelize';
 import { Model } from "sequelize/types";
